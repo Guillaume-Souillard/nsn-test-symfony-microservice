55c18ac6dcd3385b36768bb2c11a541e28c9697f
guillaumesouillard <guillaume.souillard@gmail.com>
2025-03-07 14:47:00 +0100

    first commit

---
+++ src/ApiProvider/CryptoProviderInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\ApiProvider;
+
+interface CryptoProviderInterface
+{
+
+}
---
+++ tests/ApiProvider/CoinMarketCapProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Tests\ApiProvider;
+
+class CoinMarketCapProviderTest
+{
+
+}
